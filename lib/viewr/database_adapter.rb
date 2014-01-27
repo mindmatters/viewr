61be867026f9e11b0e0268f2fc439b9047320d55
Frank Ploss <frank@fqxp.de>
2014-01-27 16:54:22 +0100

    drop all functions by name and arguments

--- lib/viewr/database_adapter.rb
+++ lib/viewr/database_adapter.rb
@@ -15,15 +15,32 @@ module Viewr
     end
 
     def drop_function(function_name)
-      run(drop_function_sql(function_name))
+      existing_functions_with_argument_types(function_name).each do |function_with_argument_types|
+        run(drop_function_sql(function_with_argument_types))
+      end
     end
 
     def drop_view_sql(view_name)
       "DROP VIEW IF EXISTS #{view_name} CASCADE"
     end
 
-    def drop_function_sql(function_name)
-      "DROP FUNCTION IF EXISTS #{function_name} CASCADE"
+    def drop_function_sql(function_with_argument_types)
+      "DROP FUNCTION IF EXISTS #{function_with_argument_types} CASCADE"
+    end
+
+    def existing_functions_with_argument_types(function_name)
+      functions = []
+      @connection["
+        SELECT
+          nspname,
+          proname,
+          oidvectortypes(proargtypes) AS args
+        FROM pg_proc
+        INNER JOIN pg_namespace ON (pg_namespace.oid = pg_proc.pronamespace)
+        WHERE (proname = '#{function_name}')"].each do |r|
+          functions << "#{r[:nspname]}.#{r[:proname]} (#{r[:args]})"
+        end
+      functions
     end
   end
 end
