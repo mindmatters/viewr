0d8f23c5a22edf0615213304552d97fd8e0566ad
Luciano Maiwald <luciano.maiwald@gmail.com>
2015-08-07 15:23:37 +0200

    update rspec to ~> 3.3.0

--- spec/viewr/database_adapter_spec.rb
+++ spec/viewr/database_adapter_spec.rb
@@ -7,13 +7,13 @@ describe Viewr::DatabaseAdapter do
 
   describe '#initialize' do
     it 'saves the given connection to an attribute' do
-      database_adapter.connection.should == connection
+      expect(database_adapter.connection).to eql(connection)
     end
   end
 
   describe '#run' do
     it 'runs the given statement on its inner connection' do
-      connection.should_receive(:run).with(:sql_statement)
+      expect(connection).to receive(:run).with(:sql_statement)
 
       database_adapter.run(:sql_statement)
     end
@@ -21,8 +21,8 @@ describe Viewr::DatabaseAdapter do
 
   describe '#drop_view' do
     it 'runs the SQL statement returned by #drop_view_sql' do
-      database_adapter.should_receive(:drop_view_sql).with(:view_name).and_return(:sql_statement)
-      database_adapter.should_receive(:run).with(:sql_statement)
+      expect(database_adapter).to receive(:drop_view_sql).with(:view_name).and_return(:sql_statement)
+      expect(database_adapter).to receive(:run).with(:sql_statement)
 
       database_adapter.drop_view(:view_name)
     end
@@ -30,9 +30,9 @@ describe Viewr::DatabaseAdapter do
 
   describe '#drop_function' do
     it 'runs the SQL statement returned by #drop_function_sql' do
-      database_adapter.should_receive(:existing_functions_with_argument_types).with(:function_name).and_return([:foo])
-      database_adapter.should_receive(:drop_function_sql).with(:foo).and_return(:sql_statement)
-      database_adapter.should_receive(:run).with(:sql_statement)
+      expect(database_adapter).to receive(:existing_functions_with_argument_types).with(:function_name).and_return([:foo])
+      expect(database_adapter).to receive(:drop_function_sql).with(:foo).and_return(:sql_statement)
+      expect(database_adapter).to receive(:run).with(:sql_statement)
 
       database_adapter.drop_function(:function_name)
     end
@@ -41,13 +41,13 @@ describe Viewr::DatabaseAdapter do
 
   describe '#drop_view_sql' do
     it 'returns an SQL statement to drop the given view' do
-      database_adapter.drop_view_sql('view_name').should == 'DROP VIEW IF EXISTS view_name CASCADE'
+      expect(database_adapter.drop_view_sql('view_name')).to eql('DROP VIEW IF EXISTS view_name CASCADE')
     end
   end
 
   describe '#drop_function_sql' do
     it 'returns an SQL statement to drop the given function' do
-      database_adapter.drop_function_sql('function_name').should == 'DROP FUNCTION IF EXISTS function_name CASCADE'
+      expect(database_adapter.drop_function_sql('function_name')).to eql('DROP FUNCTION IF EXISTS function_name CASCADE')
     end
   end
 end
--- spec/viewr_spec.rb
+++ spec/viewr_spec.rb
@@ -8,8 +8,9 @@ describe Viewr do
   describe '.create_all' do
     it 'sets up a runner and calls #create_all on it' do
       runner = double(:runner)
-      Viewr.should_receive(:setup_runner).with(:connection, :view_files_path, :function_files_path).and_return(runner)
-      runner.should_receive(:create_all)
+
+      expect(Viewr).to receive(:setup_runner).with(:connection, :view_files_path, :function_files_path).and_return(runner)
+      expect(runner).to receive(:create_all)
 
       Viewr.create_all(:connection, :view_files_path, :function_files_path)
     end
@@ -18,8 +19,9 @@ describe Viewr do
   describe '.drop_all' do
     it 'sets up a runner and calls #create_all on it' do
       runner = double(:runner)
-      Viewr.should_receive(:setup_runner).with(:connection, :view_files_path, :function_files_path).and_return(runner)
-      runner.should_receive(:drop_all)
+
+      expect(Viewr).to receive(:setup_runner).with(:connection, :view_files_path, :function_files_path).and_return(runner)
+      expect(runner).to receive(:drop_all)
 
       Viewr.drop_all(:connection, :view_files_path, :function_files_path)
     end
@@ -28,9 +30,9 @@ describe Viewr do
   describe '.recreate_all' do
     it 'sets up a runner, then drops all database objects, then re-creates them' do
       runner = double(:runner)
-      Viewr.should_receive(:setup_runner).with(:connection, :view_files_path, :function_files_path).and_return(runner)
-      runner.should_receive(:drop_all).ordered
-      runner.should_receive(:create_all).ordered
+      expect(Viewr).to receive(:setup_runner).with(:connection, :view_files_path, :function_files_path).and_return(runner)
+      expect(runner).to receive(:drop_all).ordered
+      expect(runner).to receive(:create_all).ordered
 
       Viewr.recreate_all(:connection, :view_files_path, :function_files_path)
     end
@@ -40,10 +42,10 @@ describe Viewr do
     it 'creates a database object runner, then loads views and functions into it' do
       adapter = double(:database_adapter)
       runner = double(:database_object_runner)
-      Viewr::DatabaseAdapter.should_receive(:new).with(:connection).and_return(adapter)
-      Viewr::SchemaObjectRunner.should_receive(:new).and_return(runner)
-      Viewr.should_receive(:load_views).with(:view_files_path, runner, adapter)
-      Viewr.should_receive(:load_functions).with(:function_files_path, runner, adapter)
+      expect(Viewr::DatabaseAdapter).to receive(:new).with(:connection).and_return(adapter)
+      expect(Viewr::SchemaObjectRunner).to receive(:new).and_return(runner)
+      expect(Viewr).to receive(:load_views).with(:view_files_path, runner, adapter)
+      expect(Viewr).to receive(:load_functions).with(:function_files_path, runner, adapter)
 
       Viewr.setup_runner(:connection, :view_files_path, :function_files_path)
     end
@@ -54,9 +56,10 @@ describe Viewr do
       runner = double(:database_object_runner)
       adapter = double(:database_adapter)
       yaml_data = double(:yaml_data)
-      Viewr::View.should_receive(:new_from_yaml).with(
-        IO.read('spec/fixtures/views/example_view.yml'), adapter).and_return(yaml_data)
-      runner.should_receive(:<<).with(yaml_data)
+      expect(Viewr::View).to receive(:new_from_yaml)
+        .with(IO.read('spec/fixtures/views/example_view.yml'), adapter)
+        .and_return(yaml_data)
+      expect(runner).to receive(:<<).with(yaml_data)
 
       Viewr.load_views('spec/fixtures/views', runner, adapter)
     end
@@ -67,9 +70,10 @@ describe Viewr do
       runner = double(:database_object_runner)
       adapter = double(:database_adapter)
       yaml_data = double(:yaml_data)
-      Viewr::Function.should_receive(:new_from_yaml).with(
-        IO.read('spec/fixtures/functions/example_function.yml'), adapter).and_return(yaml_data)
-      runner.should_receive(:<<).with(yaml_data)
+      expect(Viewr::Function).to receive(:new_from_yaml)
+        .with(IO.read('spec/fixtures/functions/example_function.yml'), adapter)
+        .and_return(yaml_data)
+      expect(runner).to receive(:<<).with(yaml_data)
 
       Viewr.load_functions('spec/fixtures/functions', runner, adapter)
     end
